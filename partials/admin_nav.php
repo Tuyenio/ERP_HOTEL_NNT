7d262b1969a88aabb3f649a4a472f6009408cda1
Nguyễn Ngọc Tuyền <tt98tuyen@gmail.com>
2025-05-16 04:06:55 +0700

    Update admin navigation labels and improve accessibility attributes

--- partials/admin_nav.php
+++ partials/admin_nav.php
@@ -1,26 +1,26 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
+    <!-- Thanh điều hướng bên trái -->
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
 
+    <!-- Thanh điều hướng bên phải -->
     <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
+            <a class="nav-link" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
                 <i class="far fa-user"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <div class="dropdown-divider"></div>
                 <a href="profile.php" class="dropdown-item">
-                    <i class="fas fa-user-cog mr-2"></i> Hồ sơ của tôi
+                    <i class="fas fa-user-cog mr-2"></i> Thông tin cá nhân
                 </a>
                 <div class="dropdown-divider"></div>
                 <a href="logout.php" class="dropdown-item">
                     <i class="fas fa-power-off mr-2"></i> Đăng xuất
                 </a>
-
             </div>
         </li>
     </ul>
